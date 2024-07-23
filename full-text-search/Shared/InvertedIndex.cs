5341eaa698b65867f9065f6b81e3f27578f70e75
Mohammad Mahdi Rajabi <m.m.rajabi.84@gmail.com>
2024-07-23 13:19:33 +0330

    refactor: use naming conventions and shorter methods

--- full-text-search/Shared/InvertedIndex.cs
+++ full-text-search/Shared/InvertedIndex.cs
@@ -3,24 +3,22 @@ using System.Text.RegularExpressions;
 namespace Mohaymen.FullTextSearch.Shared;
 public class InvertedIndex
 {
-    private Dictionary<string, HashSet<string>> InvertedIndexMap;
+    private Dictionary<string, HashSet<string>> _invertedIndexMap;
     public InvertedIndex()
     {
-        InvertedIndexMap = new Dictionary<string, HashSet<string>>();
+        _invertedIndexMap = new Dictionary<string, HashSet<string>>();
     }
 
     public void ProcessFilesWords(Dictionary<string, string> filesContent)
     {
-        foreach(var file in filesContent)
+        foreach(var (filePath, fileText) in filesContent)
         {
-            string filePath = file.Key;
-            string fileText = file.Value;
             var words = Regex.Split(fileText, @"[^\w']+");
 
-            InvertedIndexMap = words
+            _invertedIndexMap = words
                 .Where(word => !string.IsNullOrWhiteSpace(word))
                 .Select(word => word.ToUpper())
-                .Aggregate(InvertedIndexMap, (map, upperWord) =>
+                .Aggregate(_invertedIndexMap, (map, upperWord) =>
                 {
                     if (!map.ContainsKey(upperWord))
                     {
@@ -35,7 +33,7 @@ public class InvertedIndex
     public HashSet<string> SearchWord(string word)
     {
         string upperWord = word.ToUpper();
-        InvertedIndexMap.TryGetValue(upperWord, out HashSet<string>? result);
+        _invertedIndexMap.TryGetValue(upperWord, out HashSet<string>? result);
         return result ?? new HashSet<string>();
     }
 }
