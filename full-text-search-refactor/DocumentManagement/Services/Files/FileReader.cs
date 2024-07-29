33b9fe0653551eeebb39323ae79f61b709a7ee60
Mobin Barfi <m_barfi@comp.iust.ac.ir>
2024-07-29 12:41:18 +0330

    refactor: Extract AddFileToCollection method

--- full-text-search-refactor/DocumentManagement/Services/Files/FileReader.cs
+++ full-text-search-refactor/DocumentManagement/Services/Files/FileReader.cs
@@ -7,15 +7,19 @@ public class FileReader : IFileReader
     public FileCollection ReadAllFiles(string folderPath)
     {
         var files = Directory.GetFiles(folderPath);
-        var fileCollection = files.Aggregate(new FileCollection(), (collection, filePath) =>
-            {
-                if (!collection.ContainsFile(filePath))
-                    collection.AddFile(filePath,
-                    File.ReadAllText(filePath));
-
-                return collection;
-            });
+        var fileCollection = files.Aggregate(new FileCollection(), AddFileToCollection);
 
         return fileCollection;
+    }
+
+    private FileCollection AddFileToCollection(FileCollection collection, string filePath)
+    {
+        if (!collection.ContainsFile(filePath))
+            collection.AddFile(
+                filePath,
+                File.ReadAllText(filePath)
+            );
+
+        return collection;
     }
 }
